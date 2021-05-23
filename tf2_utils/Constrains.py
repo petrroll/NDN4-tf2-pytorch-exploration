ef55cde3e84c86e94463ec36fd300f20c1483e61
Petr Houška <houskape@gmail.com>
2021-05-23 23:08:02 +0200

    Implement minMax constraint.

---
+++ tf2_utils/Constrains.py
@@ -0,0 +1,25 @@
+from typing import Optional
+import tensorflow as tf
+
+
+class MinMax(tf.keras.constraints.Constraint):
+  """Constrains tensors to have individual-values between min and max."""
+
+  def __init__(
+      self, 
+      min: Optional[float] = None, 
+      max: Optional[float] = None
+      ):
+    self.min = min
+    self.max = max
+
+  def __call__(self, w):
+    if self.min and self.max:
+        return tf.clip_by_value(w, self.min, self.max)
+    elif self.min:
+        return tf.maximum(w, self.min)
+    elif self.max:
+        return tf.minimum(w, self.max)
+
+  def get_config(self):
+    return {'min': self.min, 'max': self.max}
